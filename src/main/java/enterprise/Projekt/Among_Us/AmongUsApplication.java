cec1319b84731d3920118ce5129d11a7b93b1ca5
Markus Elmenreich <m.elmenreich@gmail.com>
2024-03-23 09:08:10 +0100

    setup

---
+++ src/main/java/enterprise/Projekt/Among_Us/AmongUsApplication.java
@@ -0,0 +1,13 @@
+package enterprise.Projekt.Among_Us;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmongUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmongUsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/enterprise/Projekt/Among_Us/AmongUsApplicationTests.java
@@ -0,0 +1,13 @@
+package enterprise.Projekt.Among_Us;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmongUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
